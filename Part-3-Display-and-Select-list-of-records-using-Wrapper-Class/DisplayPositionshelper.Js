925a16ae512eeaf3eb870587c1f3a1d2c317c420
Vinay Chaturvedi <vinay.chaturvedi@hotmail.com>
2016-09-14 19:10:38 +0530

    Create DisplayPositionshelper.Js

---
+++ Part-3-Display-and-Select-list-of-records-using-Wrapper-Class/DisplayPositionshelper.Js
@@ -0,0 +1,24 @@
+({
+	getPosition : function(component) {
+		
+        var action = component.get("c.getPositionRecords");
+        
+        //Setting the Callback
+            action.setCallback(this,function(a){
+                //get the response state
+                var state = a.getState();
+                
+                //check if result is successfull
+                if(state == "SUCCESS"){
+                    var result = a.getReturnValue();
+                    if(!$A.util.isEmpty(result) && !$A.util.isUndefined(result))
+                    	component.set("v.lstPositions",result);
+                } else if(state == "ERROR"){
+                    alert('Error in calling server side action');
+                }
+            });
+            
+            //adds the server-side action to the queue        
+            $A.enqueueAction(action);
+	}
+})
